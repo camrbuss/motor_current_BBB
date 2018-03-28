84bf026fcf6867572004765cae639b74d8655393
Cam Buss <cam.r.buss@gmail.com>
2018-03-28 15:47:24 -0400

    Added ADC read functionality
    asdf

--- src/adc_print.cpp
+++ src/adc_print.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
-  cout << "CAM" << '\n';
+  char value_str[7];
+  long int value_int = 0;
+  cout << "ADC Read Start" << '\n';
+  FILE *ADCStream = fopen("/sys/bus/iio/devices/iio\:device0/in_voltage0_raw", "r");
+  // char *ADCvalue = "/sys/bus/iio/devices/iio\:device0/in_voltage0_raw";
+
+  for(int i=0; i<10; i++)
+  {
+    fread(&value_str, 6, 6, ADCStream);
+    value_int = strtol(value_str,NULL,0);
+    printf("0 %li\n", value_int);
+    fflush(stdout);
+    sleep(1);
+  }
+
 }
